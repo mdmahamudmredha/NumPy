a4b7a9a645deebc9985c5cb165f5e12b438fdb68
Md. Mahamud Mredha <79185427+mdmahamudmredha@users.noreply.github.com>
2025-07-24 18:07:43 +0600

    Add files via upload

---
+++ Day - 01/import_ways/all_functions.py
@@ -0,0 +1,33 @@
+# Module
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
+
+def average(a, b):
+    return (a + b) / 2
+
+def square(a):
+    return a * a
+
+def cube(a):
+    return a * a * a
+
+def modulus(a, b):
+    return a % b
+
+def power(a, b):
+    return a ** b
+
+def floor_division(a, b):
+    return a // b
---
+++ Day - 01/import_ways/main.py
@@ -0,0 +1,25 @@
+# main.py
+import all_functions
+
+print(all_functions.add(3, 5))
+print(all_functions.subtract(7, 5))
+print(all_functions.power(2, 3))
+
+
+
+
+
+from all_functions import add
+
+print(add(7, 5))
+#print(multiply(7, 5))
+
+
+
+
+from all_functions import add, multiply
+
+print(add(7, 5))
+print(multiply(7, 5))
+
+
